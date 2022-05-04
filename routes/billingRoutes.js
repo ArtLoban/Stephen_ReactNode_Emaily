6ef0ecc382f27f2e568e9177b4e427af51a44201
Artem <artem.loban@devit-team.com>
2022-05-04 17:08:08 +0300

    Stripe Charge comlited

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,16 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
 module.exports = (app) => {
-  app.post('/auth/google', (req, res) => {
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '5$. My First Test Charge (created for API docs)',
+      source: req.body.id,
+    });
+
+    console.log(charge);
 
   });
 };
